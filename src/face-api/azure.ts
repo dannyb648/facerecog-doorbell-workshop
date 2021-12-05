06f70dfabcea958c4ce6c0431f63ba1f7e88215d
Dan Beglin <dbeglinuk@gmail.com>
2021-12-05 16:54:05 +0000

    added in more code to complete repo

--- src/face-api/azure.ts
+++ src/face-api/azure.ts
@@ -1,7 +1,7 @@
 // ------------------------------- NODE MODULES -------------------------------
 
 import { ApiKeyCredentials } from "@azure/ms-rest-js";
-import  { FaceClient } from "@azure/cognitiveservices-face";
+import { FaceClient } from "@azure/cognitiveservices-face";
 import { promises } from 'fs';
 
 // ------------------------------ CUSTOM MODULES ------------------------------
@@ -14,6 +14,7 @@ const endpoint = process.env.FACEAPIENDPOINT!;
 const key = process.env.FACEAPIKEY!;
 const group = process.env.FACEAPIGROUP!;
 
+// These are the key objects Azure Faces provide as an SDK
 const credentials = new ApiKeyCredentials({ inHeader: { 'Ocp-Apim-Subscription-Key': key } });
 const client = new FaceClient(credentials, endpoint);
 
@@ -21,23 +22,64 @@ const client = new FaceClient(credentials, endpoint);
 
 export const identifyFaces = async (image: string): Promise<Identifcations | void> => {
     // Grab IDs from detectFacesFromImage('snapshot', client)
+    const faceIds = (await detectFacesFromImage(image, client)).map((face: Face) => face.faceId);
+    if (faceIds.length === 0) {
+        console.log('No face IDs');
 
-    // throw an error if no faces are detected (length = 0)
+        return;
+    }
 
     // call client.face.identity, pass in the ids and the personGroupId
+    let results;
+    try {
+        results = await client.face.identify(faceIds, { personGroupId : group});
 
-    // Process Detected Faces, 
-        // for each result, go and call client.personGroupPerson.get
+        console.log(results[0].candidates);
 
-        // grab the name of that person from the person dictionary
+    } catch (err) {
+        console.log(err);
+        throw new Error('Azure Face API is unavailable');
+    }
 
+    // Response object we are going to populate
+    const detections: Identifcations = {
+        peopleTotal: results.length,
+        people: [],
+    };
+
+    // Process Detected Faces
+    await Promise.all(results.map (async (result: any) => { 
+        if (result.candidates.length > 0) {
+            // for each result, go and call client.personGroupPerson.get
+            const person: Person = await client.personGroupPerson.get(group, result.candidates[0].personId);
+
+            const id = person.personId;
+
+            // Filter down to the name of the face
+            const [name] = peopleDict.filter(person => {
+                return person.id === id;
+            });
+
+            // Push the name onto our response object
+            detections.people.push(name.name);
+        }
+    }));
+    
+    return detections;
 }
 
 const detectFacesFromImage = async (image: string, client: any): Promise<Face[]> => {
-
     // read in snapshot taken by photo as a data stream
+    const filename = `snapshot.jpg`;
+    const stream = await promises.readFile(filename);
 
     // get all detected with client.face.detectWithStream 
     // pass in the image stream
+    const detectedFaces = await client.face.detectWithStream(stream,
+        {
+            detectionModel: "detection_02",
+            recognitionModel: "recognition_03"
+        });
 
+    return detectedFaces;
 }
--- src/face-api/faces.ts
+++ src/face-api/faces.ts
@@ -25,4 +25,29 @@ export interface Person {
     persistedFaceIds?: string[];
 }
 
-export const peopleDict = [];
+/* 
+ Youll need to create a person group, then extract the Ids and label them here
+ https://docs.microsoft.com/en-us/azure/cognitive-services/face/quickstarts/client-libraries?tabs=visual-studio&pivots=programming-language-javascript#create-a-persongroup
+*/
+export const peopleDict = [
+    {
+        id: '64bitguid from Azure PersonGroup',
+        name: 'Person A'
+    },
+    {
+        id: '64bitguid from Azure PersonGroup',
+        name: 'Person B'
+    },
+    {
+        id: '64bitguid from Azure PersonGroup',
+        name: 'Person C'
+    },
+    {
+        id: '64bitguid from Azure PersonGroup',
+        name: 'Person D'
+    },
+    {
+        id: '64bitguid from Azure PersonGroup',
+        name: 'PErson E'
+    }
+]
